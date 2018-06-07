811ab6f2100233c84878d5522ef2db34bd46f516
Kramerbaf <nds.kram@gmail.com>
2018-06-07 14:23:39 +0200

    Updated vue-cli

--- src/section.vue
+++ src/section.vue
@@ -48,35 +48,34 @@
 			</li>
 		</ul>
 		<wwObject v-bind:ww-object="section.data.title"></wwObject>
+		<wwObject v-bind:ww-object="section.data.title"></wwObject>
 	</div>
 </template>
 
 <script>
 let a = {
-	name: "feature_D",
-	props: {
-		msg: String,
-		section: Object
-	},
-	methods: {
-		lala: function () {
-			console.log("lala");
-		}
-	}
+  name: "feature_D",
+  props: {
+    msg: String,
+    section: Object
+  },
+  methods: {
+    lala: function() {
+      console.log("lala");
+    }
+  }
 };
 
 /* wwManager:start */
 const managerFunctions = require("./manager.js").default;
 for (const key in managerFunctions) {
-	if (a.methods[key]) {
-		console.log("Function already exists : ", key);
-	} else {
-		a.methods[key] = managerFunctions[key];
-	}
+  if (a.methods[key]) {
+    console.log("Function already exists : ", key);
+  } else {
+    a.methods[key] = managerFunctions[key];
+  }
 }
 /* wwManager:end */
-
-console.log(a.methods);
 
 export default a;
 </script>
